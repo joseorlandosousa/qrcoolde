970a47f0a1a859852f46837c9301158741952ef9
José Orlando Sousa <zeorlando@localhost.localdomain>
2020-11-12 20:46:51 -0300

    adicionada base da camera

--- App.js
+++ App.js
@@ -2,19 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import QRCamera from './components/Camera/QRCamera';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QRCamera style={StyleSheet.absoluteFillObject}></QRCamera>      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
