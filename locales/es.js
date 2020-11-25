ae7abb73891278e3ac50ce8336c87d8e284378ea
José Orlando Sousa <zeorlando@localhost.localdomain>
2020-11-25 20:18:30 -0300

    adicionado espanhol

---
+++ locales/es.js
@@ -0,0 +1,48 @@
+export default {
+    // types of data
+    contact: "Contacto",
+    phone: "Número de teléfono",
+    sms: "SMS",
+    email: "E-mail",
+    location: "Ubicación",
+    wifi: "Wifi",
+    whatsapp: "WhatsApp",
+    url: "URL",
+    text: "Texto",
+
+    // labels
+    message: "Mensaje",
+    subject: "Asunto",
+    latitude: "Latitud",
+    longitude: "Longitud",
+    network: "Red",
+    password: "Contraseña",
+    cript: "Criptografía",
+    name: "Nombre",
+    cel: "Numero de celular",
+    org: "Organización",
+
+    //
+    copied: "Copiado",
+
+
+    btn: {
+        contact: "Agregar contacto",
+        phone: "Llamada",
+        sms: "Enviar SMS",
+        email: "E-mail",
+        location: "Abrir en mapa",
+        wifi: "Copiar contraseña",
+        whatsapp: "Enviar WhatsApp",
+        url: "Abierta en el navegador",
+        text: "Copiar texto",
+        camPermission: "Habilita la cámara"
+    },
+
+    camPermission: {
+        title: "Cámara sin permiso",
+        description: "La cámara se usa solo para leer el QRCode."
+    }
+
+
+}
